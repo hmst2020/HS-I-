ae0e73d758c47c3dc52282137e2fe666eb57dc38
hmst2020 <60832331+hmst2020@users.noreply.github.com>
2021-10-09 17:07:18 +0800

    Add files via upload

--- R/2_1_v01.R
+++ R/2_1_v01.R
@@ -1,4 +1,5 @@
 ####### 繪製台北市界地圖解法一###################
+library(tibble)
 library(geojsonio)  # 載入處理GeoJSON資料套件
 geojson.sp <- geojson_read( # 將下載之縣市GeoJSON圖資讀入變數
   x= 'https://raw.githubusercontent.com/g0v/twgeojson/master/json/twCounty2010.geo.json',
@@ -52,25 +53,74 @@ g <- ggplot(  # 使用繪圖函式產生繪圖物件
   geom_path(show.legend=FALSE)  # 繪出座標路徑線
 print(g) # 印出台北縣市界地圖
 
+
+
 ############土壤液化分布標示於地圖上######################
-taipei.js <- geojson_read( # 將下載之縣市GeoJSON圖資讀入變數
-  x='data/taipei_soil.geojson',
-  what = "sp" # 指定回傳Spatial class 之物件
+library(geojsonR)
+taipei.sev<-FROM_GeoJson(
+  url_file_string=     # 開放資料源
+    'https://soil.taipei/Taipei/Main/pages/TPLiquid_84.GeoJSON',
+  To_List=TRUE   # 指定傳回各子物件皆為list
 )
-soil.df <- tidy( # 將sp物件轉換為data.frame物件
-  taipei.js)  # 資料對象
-xclass<- taipei.js$class
-sev<-function(i){xclass[i]}
-library(dplyr)
-soil.df<-soil.df %>%
-  mutate(severity=
-           sev(as.integer(id)))
-head(soil.df)
+rshp.f <- function(
+  result,     # soil.df疊加物件
+  polygons,   # Multipolygon(list)物件
+  grp,        # group 編號
+  severity){ # 宣告處裡繪圖data.frame圖資之自訂函式
+  long<-c() # 初始經度vector 
+  lat<-c()  # 初始緯度vector
+  if(is.list(polygons[[1]])){  # 判斷是否包含多個 polygon
+    for (j in 1:length(polygons)){   # 迴圈每個polygon
+      result<-rshp.f(   # 
+          result,
+          polygons[[j]],      # 第j個polygon
+          paste0(grp,'.',j),  # 同一個polygon群組編號唯一且相同
+          severity)
+    }
+    return (result)
+  }else if(is.vector(polygons[[1]])){
+    for (j in 1:length(polygons)){
+      coord<-polygons[[j]]
+      long<-coord[1]  # 經度資料
+      lat<-coord[2]   # 緯度資料
+      result <- rbind( 
+        result,
+        tibble( # 產生新的data.frame物件
+          long=long, # 指定經度欄位vector資料
+          lat=lat,   # 指定緯度欄位vector資料
+          group=grp, # 指定封閉區塊(polygon)的歸群(group)資料
+          severity=severity # 指定填(fill)色之顏色代表嚴重程度
+        )) 
+    }
+    return (result)
+  }else{
+    stop('format exception occured !')
+  }
+}
+soil.df<-tibble() # 初始soil.df為data.frame物件
+features<-taipei.sev$features   # 取出features物件
+for (i in 1:length(features)){  # 迴圈features的每一多邊形物件
+  feature<-features[[i]]
+  severity<-feature$properties$class
+  type<-feature$geometry$type
+  coords<-feature$geometry$coordinates[[1]]
+  if (type=='MultiPolygon'){   # 判斷是否為多邊形物件
+    polygons<-coords
+    soil.df<-rshp.f(  # 展開多邊形座標，同一多邊形物件的grp引數相同
+      soil.df,
+      polygons,   # 多邊形物件(可能1個以上)
+      i,          # grp引數
+      severity)
+  }else{
+    stop('NonmultiPolygon exception occured !')
+  }
+}
+head(soil.df)  # 列印前6筆
 map.title <- "台北市土壤液化潛勢分布" # 圖表標題
 lgnd.title <- '嚴重程度' # 圖例標題
 class.name <- c('High','Medium','Low') # 圖例顏色對應(嚴重度)
 colors= c('red','#FFFF00','green') # 填色顏色對應
-g<-g+ # 將上述的g物件累加多邊形的土壤潛勢資料
+p<-g+ # 將上述的g物件累加多邊形的土壤潛勢資料
   geom_polygon( # 繪出多邊形資料
     data = soil.df, # 繪圖資料
     mapping=aes( # 指定data 的欄位
@@ -89,5 +139,5 @@ g<-g+ # 將上述的g物件累加多邊形的土壤潛勢資料
   labs(title=map.title,  # 設定圖表名稱及xy各軸標籤
        x ="經度", y = "緯度")+ 
   theme_bw() # 繪圖主題使用黑白
-print(g) # 印出圖
+print(p) # 印出市界及潛勢分布圖
 ##############  end of 2_1_v01.R ##########
